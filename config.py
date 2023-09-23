2d80b96a216f68b8f12dbcec4f9593d67f6a24ea
Larry <lazybear17@gmail.com>
2023-09-23 20:44:40 +0800

    add classes in config.py, evaluate CCIPModel in module_test.ipynb

--- config.py
+++ config.py
@@ -19,3 +19,8 @@ OBJ2IDX = {
 IDX2ATR = {v : k for k, v in ATR2IDX.items()}
 
 IDX2OBJ = {v : k for k, v in OBJ2IDX.items()}
+
+classes = []
+for va in IDX2ATR.values():
+    for vo in IDX2OBJ.values():
+        classes.append(f"{va} {vo}")
