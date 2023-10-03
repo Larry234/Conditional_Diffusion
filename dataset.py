21d2c23893831b177a57e3d5a9080ba19ebeaa0f
Larry234 <lazybear17@gmail.com>
2023-10-03 22:21:38 +0800

    add CustomImageDatasetTripleCond for training with three labels

--- dataset.py
+++ dataset.py
@@ -103,6 +103,39 @@ class CustomImageDataset(Dataset):
     
     def get_class(self):
         return self.obj_dict, self.atr_dict
+    
+class CustomImageDatasetTripleCond(Dataset):
+    def __init__(self, root, transform=None, ignored=None):
+        self.root = root
+        self.transform = transform
+        
+        images = glob(os.path.join(root, '**', '*.jpg'))
+        self.image_path = []
+        self.labels = []
+        for image in images:
+            cat = image.split('/')[-2]
+            if ignored == None or cat not in ignored:
+                size, atr, obj = cat.split(" ")
+                self.image_path.append(image)
+                self.labels.append((size, atr, obj))
+                
+        self.classes = set(self.labels)             
+                
+    def __len__(self):
+        return len(self.image_path)
+
+    def __getitem__(self, index):
+        if torch.is_tensor(index):
+            index = index.tolist()
+        image = Image.open(self.image_path[index]).convert('RGB')
+        label = self.image_path[index].split('/')[-2]
+        label = label.split(' ')
+        
+        if self.transform is not None:
+            image = self.transform(image)
+        
+        return {"image": image, "size": self.labels[index][0], "atr": self.labels[index][1], "obj": self.labels[index][2]}
+    
 
 class CustomSampler(Sampler):
     def __init__(self, data):
